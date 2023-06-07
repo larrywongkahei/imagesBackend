716b674ba6a1d8d31aafa5c1e14e38492adf20ff
Maryhei <makemak123@gmail.com>
2023-06-07 15:51:11 +0100

    Added two json file and added product model

--- API/Model/Product.cs
+++ API/Model/Product.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+
 namespace API.Model
 {
 	public class Product
 	{
+		[JsonPropertyName("Name")]
+		public string Name { get; set; }
+
+        [JsonPropertyName("Brand")]
+        public string Brand { get; set; }
+
+        [JsonPropertyName("Price")]
+        public double Price { get; set; }
+
+        [JsonPropertyName("Description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("Category")]
+        public string Category { get; set; }
+
+        [JsonPropertyName("Rating")]
+        public double Rating { get; set; }
+
+        [JsonPropertyName("Image")]
+        public string Image { get; set; }
+
 		public Product()
 		{
 		}
