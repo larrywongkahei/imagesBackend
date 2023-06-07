0a0212afd276a96dde832bb4862cbad0d45989b8
Maryhei <makemak123@gmail.com>
2023-06-07 20:19:04 +0100

    Scaffolded two new api controller to try

---
+++ API/Controllers/ProductsAPIController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsAPIController : ControllerBase
+    {
+        // GET: values
+        [HttpGet]
+        public string Get()
+        {
+            Product product = new Product();
+            return product.getAll();
+        }
+
+        // GET values/5
+        [HttpGet("{id}")]
+        public string? Get(int id)
+        {
+            Product product = new Product();
+            return product?.getNumbersOfProduct(id);
+        }
+    }
+}
---
+++ API/Controllers/UsersAPIController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersAPIController : ControllerBase
+    {
+        // GET: values
+        [HttpGet]
+        public string Get()
+        {
+            User user = new User();
+            return user.getAll();
+        }
+
+        // GET values/5
+        [HttpGet("{id}")]
+        public string? Get(int id)
+        {
+            User user = new User();
+            return user?.getNumbersOfUsers(id);
+        }
+    }
+}
